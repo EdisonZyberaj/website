77855ab28e3496de4edc04a3113de1889c0c7394
Edison <edisonzyberaj@gmail.com>
2025-07-12 14:40:30 +0200

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<div>
+			<h1 className="text-red-500 md:text-blue-500">Edison Zyberaj</h1>
+		</div>
+	);
+}
+
+export default App;
